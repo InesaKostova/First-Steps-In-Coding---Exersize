b264ac87c23ea95500072fbb7d7fc86e5a29f4c4
Inesa Kostova <kostovainesa@gmail.com>
2025-02-04 15:33:23 +0200

    Add files via upload

---
+++ Deposit calculator.cs
@@ -0,0 +1,8 @@
+﻿double deposit = double.Parse(Console.ReadLine());
+int mounth = int.Parse(Console.ReadLine());
+double percentPerYear = double.Parse(Console.ReadLine());
+
+double sum = deposit + mounth * ((deposit * percentPerYear/100) /12);
+Console.WriteLine(sum);
+
+
