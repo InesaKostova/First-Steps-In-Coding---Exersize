7612a63f63ed5a636dcad0f0bcac0f2a019c90fd
Inesa Kostova <kostovainesa@gmail.com>
2025-02-05 14:46:06 +0200

    Add files via upload

---
+++ Food Delivery.cs
@@ -0,0 +1,11 @@
+﻿int chikenMenu = int.Parse(Console.ReadLine());
+int fishMenu = int.Parse(Console.ReadLine());
+int veganMenu = int.Parse(Console.ReadLine());
+
+double priceChikenMenu = chikenMenu * 10.35;
+double priceFishMenu = fishMenu * 12.40;
+double priceVeganMenu = veganMenu * 8.15;
+
+double fullPriceMenus = priceChikenMenu + priceFishMenu + priceVeganMenu;
+double priceDesert = fullPriceMenus * 0.2;
+Console.WriteLine(fullPriceMenus + priceDesert + 2.50);
