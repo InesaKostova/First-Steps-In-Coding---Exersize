70c7af6bf927ac132689212c79a54697635d6223
Inesa Kostova <kostovainesa@gmail.com>
2025-02-06 17:19:41 +0200

    Add files via upload

---
+++ Basketball Equipment.cs
@@ -0,0 +1,10 @@
+﻿int priceForYear = int.Parse(Console.ReadLine());
+
+double shoes =0.6 * priceForYear ;
+double basketSet =0.8 *  shoes;
+double ball = basketSet * 0.25;
+double acsessories = ball * 0.2;
+
+double fullPriceForAll = shoes + basketSet + ball + acsessories + priceForYear;
+Console.WriteLine(fullPriceForAll);
+
