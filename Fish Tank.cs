27dd42f12a922eff17a818f088433404d27fcbe3
Inesa Kostova <kostovainesa@gmail.com>
2025-02-06 18:14:21 +0200

    Add files via upload

---
+++ Fish Tank.cs
@@ -0,0 +1,12 @@
+﻿int duljinaSm = int.Parse(Console.ReadLine());
+int shirinaSm = int.Parse(Console.ReadLine());
+int visochinaSm = int.Parse(Console.ReadLine());
+double procent = double.Parse(Console.ReadLine());
+
+double obemAkva = duljinaSm * shirinaSm * visochinaSm;
+double obemAkvaLitri = obemAkva / 1000;
+double nujnaVodaZapulvane = obemAkvaLitri * (1- (procent / 100));
+
+Console.WriteLine(nujnaVodaZapulvane);
+
+
